1a3315b110d7c13438093990e8acedd6ad03d192
anam8 <56875234+anam8@users.noreply.github.com>
2019-10-22 20:02:06 +0700

    Add files via upload

---
+++ JavaApplication5.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication5;
+
+/**
+ *
+ * @author ASUS
+ */
+public class JavaApplication5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    System.out.println("Nama    : Much anam ");
+    System.out.println("Jurusan : Teknik Informatika");
+    System.out.println("Kelas   : Non-Reguler");
+    System.out.println("NIM     : 201969040062");
+    System.out.println("Alamat  : betas kepulungan Kab. Pasuruan");
+    }
+    
+}
